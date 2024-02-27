f94e056c09167368c3fe137e0d396ad5ec993c34
yongri-li <yongri.liii+1@gmail.com>
2024-02-27 19:31:51 +0000

    Update from Shopify for theme uhren2000/main
    Committed from shop: Uhren2000

--- assets/custom-script.js
+++ assets/custom-script.js
@@ -1,6 +1,10 @@
-document.querySelector(".horizontal-nav:nth-child(2) > .sub:first-child").addEventListener("mousemove",function(){
-  document.documentElement.classList.add("nav-hover");
-});
-document.querySelector(".horizontal-nav:nth-child(2) > .sub:first-child").addEventListener("mouseout",function(){
-  document.documentElement.classList.remove("nav-hover");
-});
+var elem=document.querySelector(".horizontal-nav:nth-child(2) > .sub:first-child");
+if(elem)
+{
+  elem.addEventListener("mousemove",function(){
+    document.documentElement.classList.add("nav-hover");
+  });
+  elem.addEventListener("mouseout",function(){
+    document.documentElement.classList.remove("nav-hover");
+  });
+}
